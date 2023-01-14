9f2945d55e3b280010f814e935dd8e2774dd7274
danaisboss <yjk9313@gmail.com>
2023-01-15 02:18:51 +0900

    updqte

---
+++ src/pages/e26dee9b5d2347958a892ee42e3f6872.vue
@@ -0,0 +1,23 @@
+<template>
+    <NotionRenderer :blockMap="blockMap" :blockTable="blockTable" fullPage />
+  </template>
+  
+  <script>
+  import { getPageBlocks, NotionRenderer } from "vue-notion";
+  
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null, fullPage:null }),
+  
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("03b76f42f2364d0897f0a0b8d8986795");
+    },
+  
+  };
+  </script>
+  
+  <style>
+  @import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+  </style>
+  
