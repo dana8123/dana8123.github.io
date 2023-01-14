8822606bfb641f8280b4e2ef8945e17a29d0f778
danaisboss <yjk9313@gmail.com>
2023-01-15 01:40:55 +0900

    update

---
+++ src/pages/kafka.vue
@@ -0,0 +1,24 @@
+<template>
+    <NotionRenderer :blockMap="blockMap" :blockTable="blockTable" fullPage />
+    <!-- <NotionRenderer :blockMap="blockMap" :blockTable="blockTable"></NotionRenderer> -->
+  </template>
+  
+  <script>
+  import { getPageBlocks, NotionRenderer } from "vue-notion";
+  
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null, fullPage:null }),
+  
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("7d754ec89b0148eaa1f89a8fa2bbd925");
+    },
+  
+  };
+  </script>
+  
+  <style>
+  @import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+  </style>
+  
---
+++ src/pages/postgresql.vue
@@ -0,0 +1,24 @@
+<template>
+    <NotionRenderer :blockMap="blockMap" :blockTable="blockTable" fullPage />
+    <!-- <NotionRenderer :blockMap="blockMap" :blockTable="blockTable"></NotionRenderer> -->
+  </template>
+  
+  <script>
+  import { getPageBlocks, NotionRenderer, getPageTable } from "vue-notion";
+  
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null, fullPage:null }),
+  
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("d46ece1b435c41d593cf1709a939741e");
+    },
+  
+  };
+  </script>
+  
+  <style>
+  @import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+  </style>
+  
