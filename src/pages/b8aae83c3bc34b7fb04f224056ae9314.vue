8c941940d99a89a84fa7d290b355fa66c2da2fbb
dana8123 <yjk9313@gmail.com>
2023-01-27 16:01:02 +0900

    update

---
+++ src/pages/b8aae83c3bc34b7fb04f224056ae9314.vue
@@ -0,0 +1,24 @@
+<template>
+    <NotionRenderer :blockMap="blockMap" :blockTable="blockTable" fullPage />
+    <!-- <NotionRenderer :blockMap="blockMap" :blockTable="blockTable"></NotionRenderer> -->
+  </template>
+  
+  <script>
+  import { getPageBlocks, NotionRenderer } from "vue-notion";
+  
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null, fullPage:null }),
+  
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("b8aae83c3bc34b7fb04f224056ae9314");
+    },
+  
+  };
+  </script>
+  
+  <style>
+  @import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+  </style>
+  
