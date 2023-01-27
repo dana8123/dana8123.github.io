2c0fcb366d8448631f1090a9d48218f4235c2c0a
dana8123 <yjk9313@gmail.com>
2023-01-27 15:56:43 +0900

    update

---
+++ src/pages/firebase.vue
@@ -0,0 +1,23 @@
+<template>
+    <NotionRenderer :blockMap="blockMap" :blockTable="blockTable" fullPage />
+  </template>
+  
+  <script>
+  import { getPageBlocks, NotionRenderer } from "vue-notion";
+  
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null, fullPage:null }),
+  
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("b49ebd94518d48ae934b82acb2e8d1ec");
+    },
+  
+  };
+  </script>
+  
+  <style>
+  @import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+  </style>
+  
